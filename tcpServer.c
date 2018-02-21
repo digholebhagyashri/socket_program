31d046f2e864298fe6ffc24599a5280d1fd5f078
testuser <digholebhagyashri@gmail.com>
2018-02-20 23:52:31 -0500

    my new commit

--- tcpServer.c
+++ tcpServer.c
@@ -19,7 +19,25 @@ socklen_t addr_size;
 char buffer[1024];
 
 socketfd=socket(AF_INET, SOCK_STREAM,0);
+//printf("%d",socketfd);
 printf("socket created successfully\n");
+
+serverAddr.sin_family=AF_INET;
+serverAddr.sin_port=htons(PORT);
+serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+bind(socketfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+printf("bind to %d port no.\n",PORT);
+
+listen(socketfd,5);
+printf("listening\n");
+
+newSocket = accept(socketfd,(struct sockaddr*)&newAddr,&addr_size);
+strcpy(buffer,"hello");
+send(newSocket, buffer, strlen(buffer), 0);
+printf("[+]Closing the connection.\n");
+
+
 	 
 
 }
